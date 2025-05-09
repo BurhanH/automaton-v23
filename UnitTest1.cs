fcdc78280c5c7a724c2c99b0af66b5aa24343ef7
Baur Urazalinov <BurhanH@users.noreply.github.com>
2025-05-09 14:40:06 -0500

    initial

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace automaton_v23;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+    
+    [TestMethod]
+    public void TestMethod2()
+    {
+        Assert.IsTrue(false);
+    }
+}
